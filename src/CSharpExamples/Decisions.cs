c374f35006cc7c51cd82bc58d9def5457a4f5a06
Matthew Burke <matthewpatrickburke@gmail.com>
2017-03-08 14:49:52 +0000

    rearranged into classes and added decisions

---
+++ src/CSharpExamples/Decisions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tutorials
+{
+    public class Decisions
+    {
+        public void Guess(int userGuess)
+        {
+            var correctAnswer = new Random().Next(4);
+            if (userGuess == correctAnswer)
+            {
+                Console.WriteLine("Correct");
+            }
+            else if (userGuess < correctAnswer)
+            {
+                Console.WriteLine("Too low");
+            }
+            else
+            {
+                Console.WriteLine("Too high");
+            }
+        }
+        public void Switcher()
+        {
+            switch (1)
+            {
+                case 0:
+                    Console.WriteLine("Invalid guess.");
+                    break;
+                case 1:
+                case 2:
+                case 3:
+                    Console.WriteLine("You guessed low.");
+                    break;
+                case 4:
+                    Console.WriteLine("You guessed correct.");
+                    break;
+                default:
+                    Console.WriteLine("You guessed high.");
+                    break;
+            }
+        }
+    }
+}
---
+++ src/CSharpExamples/Program.cs
@@ -0,0 +1,18 @@
+﻿// https://www.microsoft.com/net/tutorials/csharp/getting-started
+using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var stringTutorial = new Tutorials.StringTutorial();
+            var dateTutorial = new Tutorials.DateTutorial();
+            var decisionTutorial = new Tutorials.Decisions();
+            var userGuess = Console.ReadLine();
+            decisionTutorial.Guess(int.Parse(userGuess));
+            Console.ReadLine();
+        }
+    }
+}
